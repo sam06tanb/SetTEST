0254510751c03adeda7296018c0bc154d961db52
Sam06tanb <78392140+sam06tanb@users.noreply.github.com>
2024-06-24 17:59:30 -0300

    Add files via upload

---
+++ Set/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Set<String> set = new HashSet<>();
+
+        set.add("TV");
+        set.add("Notebook");
+        set.add("Tablet");
+
+        System.out.println(set.contains("Notebook"));
+
+        for (String p : set) {
+            System.out.println(p);
+        }
+
+
+
+        }
+    }
